021c9ece10c6da6eaa43f43f71af56053d94d0bd
dl364823 <146682379+dl364823@users.noreply.github.com>
2024-01-17 20:08:07 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,7 @@
 // setup() is called once at page-load
 function setup() {
     createCanvas(800,600); // make an HTML canvas element width x height pixels
+    angleMode(DEGREES);
 }
 
 // draw() is called 60 times per second
@@ -10,11 +11,27 @@ function draw() {
     let sec = second();
 
     background(225);
-    textSize(32);
-    fill(180);
-    text(hr, 10, 30);
-    fill(100);
-    text(min, 10, 60);
-    fill(0);
-    text(sec, 10, 90);
+   
+// Hour
+  let hrColor = map(hr, 0, 23, 0, 255);
+  fill(hrColor, 100, 100);
+  ellipse(width / 2, height / 2, 200, 200);
+    
+// Min 
+  push();
+  translate(width / 2, height / 2);
+  rotate(map(min, 0, 60, 0, 360));
+  stroke(0, 0, 0);
+  line(0, 0, 100, 0);
+  pop();
+    
+// Sec
+  let secSize = map(sec, 0, 59, 10, 30);
+  fill(100, 100, 255);
+  ellipse(width / 2, height / 2, secSize, secSize);
+    
+// Log Min Change
+  if (frameCount % 60 == 0) {
+        console.log(min);
+    }
 }
